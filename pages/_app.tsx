28da4f5c8524f8de5c22b8523ed69831e4c97dc0
Jake Robertson <jake@faltro.com>
2021-09-18 22:33:32 -0700

    add default background to global style

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,17 @@
 import "tailwindcss/tailwind.css";
 import type { AppProps } from "next/app";
+import config from "../tailwind.config";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <style jsx global>{`
+        body {
+          background-color: ${config.theme.extend.colors.brand.DEFAULT};
+        }
+      `}</style>
+      <Component {...pageProps} />
+    </>
+  );
 }
 export default MyApp;
