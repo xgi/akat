e9f67edc61c9d2a3b69bc6ab213c33792a9011ba
Jake Robertson <jake@faltro.com>
2021-09-25 15:43:23 -0700

    update loading spinner in info section

--- components/InfoSection.tsx
+++ components/InfoSection.tsx
@@ -13,6 +13,7 @@ import {
 import { languageFromCode } from "../utils/languages";
 import { countryNameFromCode } from "../utils/countries";
 import { formatReleaseDate } from "../utils/layout";
+import LoadingSpinner from "./LoadingSpinner";
 
 type Props = {
   id: number;
@@ -50,7 +51,10 @@ const InfoSection: React.FC<Props> = (props: Props) => {
 
   const loadData = async () => {
     const _details = await getMovie(props.id);
-    if (!_details) return;
+    if (!_details) {
+      setDetails(undefined);
+      return;
+    }
     setDetails(_details);
 
     const _alternativeTitles = await getAlternativeTitles(props.id);
@@ -122,30 +126,7 @@ const InfoSection: React.FC<Props> = (props: Props) => {
   };
 
   if (!details) {
-    return (
-      <p>
-        <svg
-          className="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-        >
-          <circle
-            className="opacity-25"
-            cx="12"
-            cy="12"
-            r="10"
-            stroke="currentColor"
-            strokeWidth="4"
-          ></circle>
-          <path
-            className="opacity-75"
-            fill="currentColor"
-            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-          ></path>
-        </svg>
-      </p>
-    );
+    return <LoadingSpinner />;
   }
 
   return (
